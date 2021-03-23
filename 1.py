caff51936a2d10a571fb9ede2b2b8d5c2234401a
brian6484 <brianlee6484@gmail.com>
2021-03-24 01:17:09 +0900

    first commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+n = int(input())
+hola = list(map(int, input().split()))
+hola.sort()
+group = 0 
+count = 0
+
+for i in range(n):
+    count += 1
+    if count >= i:
+        group += 1
+        count = 0
+print(group)
+
+
+
