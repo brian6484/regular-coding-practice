d6e6a914d5505ea7cfade53b0791e55ac5d9059d
brian6484 <brianlee6484@gmail.com>
2021-03-31 02:07:58 +0900

    prac

---
+++ 38.py
@@ -0,0 +1,26 @@
+INF = int(1e9)
+n= int(input())
+m= int(input())
+graph = [[INF]*(n+1)for _ in range(n+1)]
+
+for a in range (1,n+1):
+    for b in range (1,n+1):
+        if a ==b:
+            graph[a][b] = 0
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    #since graph[a][b] is inf, it will satisfy below condition always unless another shouter route(c) appears and updates c
+    if c <graph[a][b]:
+        graph[a][b] = c #update value in 2d list
+    # i dont get below code
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        if graph[a][b] ==INF:
+            print(0)
+        else:
+            print(graph[a][b])
+    print()
---
+++ 39.py
@@ -0,0 +1,28 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+dx =[-1,0,1,0]
+dy = [0,1,0,-1]
+
+for tc in range(int(input())):
+    n = int(input())
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int,input().split())))
+    
+    distance = [[INF] *n for _ in range(n)]
+
+    x,y=0,0
+    q = [(graph[x][y],x,y)]
+    distance[x][y] = graph[x][y]
+
+    while q:
+        dist,x,y= heapq.heappop(q)
+        if distance[x][y] < dist:
+            continue
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            
