2c73aefd7a218f7b41a7d7d7dc38166be8d945da
brian6484 <brianlee6484@gmail.com>
2021-04-05 00:48:20 +0900

    prac

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
 
 data = input()
-result = int(data[0])
-for i in range(1,len(data)):
-    num = int(data[i])
+result = int(data[0]) # first integer value 5 of 567
+for i in range(1,len(data)):  #if 567, i=1,2 
+    num = int(data[i]) # second integer value 6 of 567 cuz i = 1
     if num <= 1 or result <=1:
         result += num
     else:
