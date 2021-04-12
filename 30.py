be0f4f0adc818f44abb23af3c58a897a88fdc8c8
brian6484 <brianlee6484@gmail.com>
2021-04-12 23:55:24 +0900

    bich

---
+++ 30.py
@@ -0,0 +1,6 @@
+from bisect import bisect_left,bisect_right
+
+def count_by_range(array,left_value,right_value):
+    right_index = bisect_right(array,right_value)
+    left_index = bisect_left(array,left_value)
+    return right_index - left_index
